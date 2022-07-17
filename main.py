4128a302f6c053954988246de62d3db8b6134ada
robbie <robbielcampbell@hotmail.com>
2022-07-17 23:07:56 +0100

    dataset ready :rocket:

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from FileHandlers.FileHandler import FileHandler
 from Webscapers.SeleniumPoetryFoundation import SeleniumPoetryFoundation
 
 if __name__ == '__main__':
-    FileHandler(SeleniumPoetryFoundation(50)).write_poems_to_txt_file("./poetry_csvs/poems.txt")
-    # FileHandler.convert_file_to_csv("./poetry_csvs/poems.txt", "./poetry_csvs/poems.csv")
+    # FileHandler(SeleniumPoetryFoundation(50)).write_poems_to_txt_file("./poetry_csvs/poems.txt")
+    FileHandler.convert_file_to_csv("./poetry_csvs/poems.txt", "./poetry_csvs/poems.csv")
